dab01885a1824db26dd4fac560efd86426391a12
Alex Seriy <alex.m.seriy@gmail.com>
2025-02-24 12:10:55 -0500

    Upgraded to the lastest FastAPI.

--- binstore/main.py
+++ binstore/main.py
@@ -1,6 +1,7 @@
 from src.apis import apis
 # from src.prisma import prisma
 from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.inmemory import InMemoryBackend
@@ -8,6 +9,14 @@ from fastapi_cache.backends.inmemory import InMemoryBackend
 
 app = FastAPI()
 app.add_middleware(GZipMiddleware, minimum_size=1000)
+app.add_middleware(CORSMiddleware,
+    allow_origins=["*"],  # Change this to specific allowed origins if needed
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+
 app.include_router(apis, prefix="/apis")
 
 @app.on_event("startup")
